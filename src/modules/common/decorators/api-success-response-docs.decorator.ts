1db9878966c2bfd13d3a767913f3173cd4ccaef6
Ali Khaleghi <alikhaleghi6058@gmail.com>
2026-04-15 18:36:04 +0330

    feat: enhance ApiSuccessResponseDocs for optional models and messages

--- src/modules/common/decorators/api-success-response-docs.decorator.ts
+++ src/modules/common/decorators/api-success-response-docs.decorator.ts
@@ -1,28 +1,32 @@
-import { ApiSuccessResponse } from '@common/responses/api-success-response.dto';
 import { applyDecorators, Type } from '@nestjs/common';
 import { ApiExtraModels, ApiResponse, getSchemaPath } from '@nestjs/swagger';
 
-export function ApiSuccessResponseDocs<TModel extends Type<any>>(
-  model: TModel,
+export function ApiSuccessResponseDocs<TModel extends Type<unknown>>(
+  model?: TModel | null,
   description?: string,
   status: number = 201,
+  message?: string,
 ) {
+  const hasModel = !!model;
+  const hasMessage = !!message;
+
   return applyDecorators(
-    ApiExtraModels(ApiSuccessResponse, model),
+    ...(hasModel ? [ApiExtraModels(model)] : []),
     ApiResponse({
       status,
       description,
       schema: {
-        allOf: [
-          { $ref: getSchemaPath(ApiSuccessResponse) },
-          {
-            type: 'object',
-            properties: {
-              data: { $ref: getSchemaPath(model) },
-            },
-            required: ['data'],
-          },
-        ],
+        type: 'object',
+        properties: {
+          success: { type: 'boolean', example: true },
+          ...(hasModel && {
+            data: { $ref: getSchemaPath(model) },
+          }),
+          ...(hasMessage && {
+            message: { type: 'string', example: message },
+          }),
+        },
+        required: ['success', ...(hasModel ? ['data'] : [])],
       },
     }),
   );
