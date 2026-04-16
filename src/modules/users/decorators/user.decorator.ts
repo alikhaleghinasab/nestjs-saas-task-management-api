51141ed9dac58dab49f9abd5a19d0d0143ecf799
Ali Khaleghi <alikhaleghi6058@gmail.com>
2026-04-16 17:37:19 +0330

    feat: add ability to extract specific field from CurrentUser decorator

--- src/modules/users/decorators/user.decorator.ts
+++ src/modules/users/decorators/user.decorator.ts
@@ -1,8 +1,9 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { User } from '@users/entities/user.entity';
 
 export const CurrentUser = createParamDecorator(
-  (data: unknown, ctx: ExecutionContext) => {
-    const request = ctx.switchToHttp().getRequest();
-    return request.user;
+  <K extends keyof User>(data: K | null, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest<{ user?: User }>();
+    return data ? request.user[data] : request.user;
   },
 );
