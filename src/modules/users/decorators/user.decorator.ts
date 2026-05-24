482cd23adc840fe6e2ffda6d84bb34eca24e49bf
Ali Khaleghi <alikhaleghi6058@gmail.com>
2026-05-24 17:13:54 +0330

    refactor(auth): replace CurrentUser decorator with CurrentUserId

--- src/modules/users/decorators/user.decorator.ts
+++ src/modules/users/decorators/user.decorator.ts
@@ -1,9 +1,8 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { User } from '@users/entities/user.entity';
 
-export const CurrentUser = createParamDecorator(
-  <K extends keyof User>(data: K | null, ctx: ExecutionContext) => {
-    const request = ctx.switchToHttp().getRequest<{ user?: User }>();
-    return data ? request.user[data] : request.user;
+export const CurrentUserId = createParamDecorator(
+  (_, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest<{ userId?: string }>();
+    return request.userId;
   },
 );
