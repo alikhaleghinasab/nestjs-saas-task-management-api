fb1c32c2ee3402e7d0c3ad12908a52258bd6e3b3
Ali Khaleghi <alikhaleghi6058@gmail.com>
2026-05-28 16:59:31 +0330

    refactor(auth): update auth service tests for Argon hasher

--- src/modules/auth/services/auth.service.spec.ts
+++ src/modules/auth/services/auth.service.spec.ts
@@ -1,14 +1,13 @@
 import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UserService } from '@users/services/user.service';
-import { ConfigService } from '@nestjs/config';
-import { BcryptHasher } from '@common/security/bcrypt-hasher.service';
 import { RefreshTokenService } from './refresh-token.service';
 import { RegisterDto } from '../dto/register.dto';
 import { LoginDto } from '@auth/dto/login.dto';
 import { User } from '@users/entities/user.entity';
 import { ForbiddenException } from '@nestjs/common';
 import { InvalidCredentialsException } from '@auth/exceptions/auth.exception';
+import { ArgonHasher } from '@common/security/argon-hasher.service';
 
 jest.mock('typeorm-transactional', () => ({
   Transactional: () => () => {},
@@ -20,9 +19,8 @@ describe('AuthService', () => {
   let service: AuthService;
 
   let userService: jest.Mocked<UserService>;
-  let bcryptHasher: jest.Mocked<BcryptHasher>;
+  let argonHasher: jest.Mocked<ArgonHasher>;
   let refreshTokenService: jest.Mocked<RefreshTokenService>;
-  let configService: jest.Mocked<ConfigService>;
 
   const registerDto: RegisterDto = {
     email: 'john@example.com',
@@ -67,7 +65,7 @@ describe('AuthService', () => {
           },
         },
         {
-          provide: BcryptHasher,
+          provide: ArgonHasher,
           useValue: {
             hash: jest.fn(),
             compare: jest.fn(),
@@ -78,23 +76,14 @@ describe('AuthService', () => {
           useValue: {
             generateTokens: jest.fn(),
           },
-        },
-        {
-          provide: ConfigService,
-          useValue: {
-            get: jest.fn(),
-          },
-        },
+        }
       ],
     }).compile();
 
     service = module.get(AuthService);
     userService = module.get(UserService);
-    bcryptHasher = module.get(BcryptHasher);
+    argonHasher = module.get(ArgonHasher);
     refreshTokenService = module.get(RefreshTokenService);
-    configService = module.get(ConfigService);
-
-    configService.get.mockReturnValue({ saltRounds: 10 });
   });
 
   afterEach(() => {
@@ -104,7 +93,7 @@ describe('AuthService', () => {
   describe('register', () => {
     it('should successfully register a user and return tokens', async () => {
       // Arrange
-      bcryptHasher.hash.mockResolvedValue(hashedPassword);
+      argonHasher.hash.mockResolvedValue(hashedPassword);
       userService.createUser.mockResolvedValue(createdUser as User);
       refreshTokenService.generateTokens.mockResolvedValue(tokens);
 
@@ -112,7 +101,7 @@ describe('AuthService', () => {
       const result = await service.register(registerDto);
 
       // Assert
-      expect(bcryptHasher.hash).toHaveBeenCalledWith(registerDto.password, 10);
+      expect(argonHasher.hash).toHaveBeenCalledWith(registerDto.password);
       expect(userService.createUser).toHaveBeenCalledWith({
         ...registerDto,
         password: hashedPassword,
@@ -128,7 +117,7 @@ describe('AuthService', () => {
     it('should successfully login and return tokens', async () => {
       // Arrange
       userService.findUserForAuth.mockResolvedValue(existingUser as User);
-      bcryptHasher.compare.mockResolvedValue(true);
+      argonHasher.compare.mockResolvedValue(true);
       refreshTokenService.generateTokens.mockResolvedValue(tokens);
 
       // Act
@@ -138,7 +127,7 @@ describe('AuthService', () => {
       expect(userService.findUserForAuth).toHaveBeenCalledWith({
         email: loginDto.email,
       });
-      expect(bcryptHasher.compare).toHaveBeenCalledWith(
+      expect(argonHasher.compare).toHaveBeenCalledWith(
         loginDto.password,
         hashedPassword,
       );
@@ -172,7 +161,7 @@ describe('AuthService', () => {
     it('should throw InvalidCredentialsException on wrong password', async () => {
       // Arrange
       userService.findUserForAuth.mockResolvedValue(existingUser as User);
-      bcryptHasher.compare.mockResolvedValue(false);
+      argonHasher.compare.mockResolvedValue(false);
 
       // Act & Assert
       await expect(service.login(loginDto)).rejects.toThrow(
