1eec38e173a487e64e0ff70686c10e64849b5ded
Ali Khaleghi <alikhaleghi6058@gmail.com>
2026-05-30 15:48:45 +0330

    refactor(cache): tighten cache driver mapper typings

--- src/modules/cache/cache-driver.mapper.ts
+++ src/modules/cache/cache-driver.mapper.ts
@@ -2,10 +2,12 @@ import { RedisService } from '@rediscore/redis.service';
 import { RedisModule } from '@rediscore/redis.module';
 import { CacheDriver } from './cache-driver.enum';
 import { InMemoryCacheProvider } from './in-memory-cache.provider';
+import { CacheProviderInterface } from './cache-provider.interface';
+import { Type } from '@nestjs/common';
 
 export interface CacheDriverConfig {
-  service: new (...args: any[]) => any;
-  module?: any;
+  service: new (...args: any[]) => CacheProviderInterface;
+  module?: Type<any>;
 }
 
 export const CacheDriverMapper: Record<CacheDriver, CacheDriverConfig> = {
