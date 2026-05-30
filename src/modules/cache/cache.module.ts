503180b44b76430dade964302f29f4c1bf14a465
Ali Khaleghi <alikhaleghi6058@gmail.com>
2026-05-30 15:09:22 +0330

    refactor(cache): inline dynamic module setup in CacheModule

--- src/modules/cache/cache.module.ts
+++ src/modules/cache/cache.module.ts
@@ -1,18 +1,32 @@
-import { Module } from '@nestjs/common';
-import { resolveCacheImports, resolveCacheProviders } from './cache.resolver';
-import { CacheModuleOptions } from './cache.interface';
-import { createDynamicModule } from '@common/utils/dynamic-module.factory';
+import { DynamicModule, Module } from '@nestjs/common';
 import { CacheHelper } from './cache.helper';
 import { CacheVersionService } from './cache-version.service';
+import { CACHE_PROVIDER } from './cache.constant';
+import { CacheDriverMapper } from './cache-driver.mapper';
+import { CacheModuleOptions } from './cache.interface';
 
-const { BaseModule } = createDynamicModule<CacheModuleOptions>({
-  importsFactory: resolveCacheImports,
-  providersFactory: resolveCacheProviders,
-  exportProviders: true,
-});
+@Module({})
+export class CacheModule {
+  static forRoot(options: CacheModuleOptions): DynamicModule {
+    const config = CacheDriverMapper[options.driver];
 
-@Module({
-  providers: [CacheHelper, CacheVersionService],
-  exports: [CacheHelper, CacheVersionService],
-})
-export class CacheModule extends BaseModule {}
+    if (!config) {
+      throw new Error(`Unsupported cache driver: ${options.driver}`);
+    }
+
+    return {
+      module: CacheModule,
+      global: true,
+      imports: config.module ? [config.module] : [],
+      providers: [
+        {
+          provide: CACHE_PROVIDER,
+          useClass: config.service,
+        },
+        CacheHelper,
+        CacheVersionService,
+      ],
+      exports: [CACHE_PROVIDER, CacheHelper, CacheVersionService],
+    };
+  }
+}
