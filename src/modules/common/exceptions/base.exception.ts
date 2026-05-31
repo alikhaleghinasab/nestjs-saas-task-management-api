1a0622e10055345bce1df14fdf9d4f05fde0cdba
Ali Khaleghi <alikhaleghi6058@gmail.com>
2026-05-31 13:52:02 +0330

    refactor: simplify BaseException property initialization

--- src/modules/common/exceptions/base.exception.ts
+++ src/modules/common/exceptions/base.exception.ts
@@ -1,12 +1,11 @@
 import { HttpException } from '@nestjs/common';
 
 export class BaseException extends HttpException {
-  public readonly errorCode: string;
-  public readonly errorMessage: string | string[];
-
-  constructor(errorCode: string, httpCode: number, message: string | string[]) {
+  constructor(
+    public readonly errorCode: string,
+    httpCode: number,
+    message: string | string[],
+  ) {
     super(message, httpCode);
-    this.errorCode = errorCode;
-    this.errorMessage = message;
   }
 }
